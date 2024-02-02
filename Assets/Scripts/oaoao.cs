1fc83d640ed6fd27e69d81859787ed7fc60d5f63
K1R1EIIIKA <sedoikin.k@gmail.com>
2024-02-02 13:17:22 +0300

    Base

---
+++ Assets/Scripts/oaoao.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oaoao : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("oaoao");
+    }
+}
