3f068673e3975a24b6166dc94a4430bc620b6a1d
Fluk <aleshagavr@mail.ru>
2024-02-05 02:49:05 +0300

    Timer created

---
+++ Assets/Scripts/TheScoreFromTheTimer.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheScoreFromTheTimer : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> stars;
+    [SerializeField] private GameObject starPanel;
+    [SerializeField] private List<int> lostStarsSeconds;
+    private int countStars;
+    private int starsNow;
+    private Timer timerScript;
+    private int currentSeconds;
+
+    private void Start()
+    {
+        timerScript = GetComponent<Timer>();
+        countStars = lostStarsSeconds.Count;
+        starsNow = (stars.Count - countStars) + countStars;
+    }
+
+    public void CheckStars()
+    {
+        for (int i = 0; i < countStars; i++)
+        {
+            CheckTime();
+            if (lostStarsSeconds[i] < currentSeconds)
+            {
+                starsNow--;
+            }
+        }
+        starPanel.SetActive(true); 
+        StarsShow();
+
+    }
+
+    private void StarsShow()
+    {
+        for(int i = 0; i < starsNow; i++)
+        {
+            stars[i].SetActive(true);
+        }
+    }
+
+    private void CheckTime()
+    {
+        currentSeconds = timerScript.OutPutSeconds();
+    }
+}
