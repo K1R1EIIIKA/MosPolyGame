7964d6bb9095a1723a19c7ac4b6f2c47b39d224a
K1R1EIIIKA <sedoikin.k@gmail.com>
2024-02-06 01:55:45 +0300

    Milk prefab

--- Assets/Scripts/Kirill/Object Logic/CameraStabilizeObject.cs
+++ Assets/Scripts/Kirill/Object Logic/CameraStabilizeObject.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class CameraStabilizeObject : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    [SerializeField] private Vector3 offset = Vector3.zero;
     
     void Update()
     {
-        transform.position = target.position;
+        transform.position = target.position + offset;
         transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 }
